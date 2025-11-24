66eb2624a64b0b44dc5c71051a66ecf290004177
Aryan <aryansinghal207@gmail.com>
2025-11-24 17:03:56 +0530

    TASK-31: Implement Filter by department API

--- src/controllers/facultyController.js
+++ src/controllers/facultyController.js
@@ -1,5 +1,14 @@
 import { faculty } from "../data/faculty.js";
 
 export const getFaculty = (req, res) => {
+  const { department } = req.query;
+
+  if (department) {
+    const filtered = faculty.filter(
+      (f) => f.department.toLowerCase() === department.toLowerCase()
+    );
+    return res.json(filtered);
+  }
+
   res.json(faculty);
 };
